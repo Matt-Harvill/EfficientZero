f791da5e538eda04d20c449ac0929224e8dbf574
Matt-Harvill <m.harvill23@gmail.com>
2023-06-03 17:20:11 -0700

    Default

--- batch_experiments.sh
+++ batch_experiments.sh
@@ -3,11 +3,16 @@
 # Define the arrays
 env_type="NoFrameskip-v4"
 envs=("Breakout" "MsPacman" "DemonAttack")
+searches=("2" "4" "8")
 
 # Triple for loop
-for env in "${envs[@]}"
+for search in "${searches[@]}"
 do
-    python main.py --env "$env$env_type" --case atari \
-    --opr test --amp_type torch_amp --num_gpus 1 --load_model \
-    --model_path "Final_Results/Models_100k/$env.p"
+    for env in "${envs[@]}"
+    do
+        python main.py --env "$env$env_type" --case atari \
+        --opr test --amp_type torch_amp --num_gpus 1 --load_model \
+        --model_path "Final_Results/Models_100k/$env.p" \
+        --searches "$search"
+    done
 done
