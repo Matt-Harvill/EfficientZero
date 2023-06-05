853148e40a4b7fe51c0b22b8c88ddbcb48ce57b5
Ubuntu <ubuntu@ip-172-31-25-189.ec2.internal>
2023-06-05 19:37:37 +0000

    intermediary policy+state similarity

--- core/mcts.py
+++ core/mcts.py
@@ -49,6 +49,21 @@ class MCTS(object):
 
             results = tree.ResultsWrapper(num)
 
+            ### For state similarity #####
+            def normalize(x, axis=-1, eps=1e-5):
+                norm = np.linalg.norm(x, axis=axis, keepdims=True)
+                return x / (norm + eps)
+
+            def cosine_similarity(batch_size, num_prev_compares, past_states, curr_state):
+                past_states = past_states.reshape((batch_size, num_prev_compares, -1))
+                curr_state = curr_state.reshape((batch_size, 1, -1))
+                past_states = normalize(past_states, axis=-1)
+                curr_state = normalize(curr_state, axis=-1)
+                return (past_states * curr_state).sum(axis=-1)
+            ################################
+
+            total_similar_states_found = 0
+
             for index_simulation in range(self.config.num_simulations):
                 hidden_states = []
                 hidden_states_c_reward = []
@@ -98,36 +113,89 @@ class MCTS(object):
                 # input(current_policy)
                 reusing_node_indices = [-1 for _ in range(num)]
 
+                # # Policy Similarity
+                # if len(policy_softmax_pool) > 1:
+                #     grouped_policies_by_num = np.transpose(np.asarray(policy_softmax_pool[1:]), axes=(1, 0, 2))
+                #     # input(grouped_policies_by_num)
+                #     # print(grouped_policies_by_num.shape)
+                #     current_policy_ = np.expand_dims(current_policy, axis=1)
+                #     # input(current_policy_.shape)
+
+                #     MSE = np.mean(np.square(grouped_policies_by_num - current_policy_), axis=2)
+                #     # input(MSE.shape)
+                #     # print(f"MSE: {MSE}")
+                #     min_MSE = np.min(MSE, axis=1)
+                #     # print(f"min_MSE: {min_MSE}")
+                #     min_under_thr = min_MSE < 0.00001
+                #     # if min_under_thr.any():
+                #     #     print("threshold reached!")
+                #     # print(f"min_under_thr: {min_under_thr}")
+                #     argmin_MSE = np.argmin(MSE, axis=1)
+                #     # print(f"argmin_MSE: {argmin_MSE}")
+                #     # Policies most similar to current policy
+                #     gathered_policy = grouped_policies_by_num[np.arange(num), argmin_MSE, :]
+                #     # # print(f"gathered_policy: {gathered_policy}")
+                #     # # print(f"current_policy: {current_policy}")
+                #     x = np.expand_dims(min_under_thr, axis=1)
+                #     input(f"min_under_thr.shape{x.shape}")
+                #     new_policy = np.where(np.expand_dims(min_under_thr, axis=1), gathered_policy, current_policy)
+                #     # print(f"new_policy: {new_policy}")
+                #     input(f"new_policy shape:{new_policy.shape}")
+                #     current_policy = new_policy
+                #     reusing_node_indices = (min_under_thr * (argmin_MSE + 2) - 1).tolist()
+                # #     input(f"reusing_node_indices: {reusing_node_indices}")
+                # # else:
+                # #     input(f"hidden_state_pool[0].shape: {hidden_state_pool[0].shape}")
+
+                # policy_softmax_pool.append(current_policy)
+                # # input(policy_softmax_pool)
+                # ############################
+
+                # Similarity
                 if len(policy_softmax_pool) > 1:
                     grouped_policies_by_num = np.transpose(np.asarray(policy_softmax_pool[1:]), axes=(1, 0, 2))
-                    # input(grouped_policies_by_num)
-                    # print(grouped_policies_by_num.shape)
                     current_policy_ = np.expand_dims(current_policy, axis=1)
-                    # input(current_policy_.shape)
 
+                    ### Policy Similarity
                     MSE = np.mean(np.square(grouped_policies_by_num - current_policy_), axis=2)
-                    # input(MSE.shape)
-                    # print(f"MSE: {MSE}")
                     min_MSE = np.min(MSE, axis=1)
-                    # print(f"min_MSE: {min_MSE}")
                     min_under_thr = min_MSE < 0.00001
-                    # if min_under_thr.any():
-                    #     print("threshold reached!")
-                    # print(f"min_under_thr: {min_under_thr}")
                     argmin_MSE = np.argmin(MSE, axis=1)
-                    # print(f"argmin_MSE: {argmin_MSE}")
-                    # Policies most similar to current policy
                     gathered_policy = grouped_policies_by_num[np.arange(num), argmin_MSE, :]
-                    # print(f"gathered_policy: {gathered_policy}")
-                    # print(f"current_policy: {current_policy}")
-                    new_policy = np.where(np.expand_dims(min_under_thr, axis=1), gathered_policy, current_policy)
-                    # print(f"new_policy: {new_policy}")
+                    
+                    ### State Similarity
+                    batch_size = grouped_policies_by_num.shape[0]
+                    num_prev_compares = grouped_policies_by_num.shape[1]
+                    cos_sims = np.zeros((batch_size, num_prev_compares, 1))
+                    grouped_hidden_states_by_num = np.transpose(np.asarray(hidden_state_pool[1:-1]), axes=(1, 0, 2, 3, 4))
+                    cos_sims = cosine_similarity(batch_size, num_prev_compares, grouped_hidden_states_by_num, hidden_state_nodes)
+                    max_cos_sim = np.max(cos_sims, axis=1)
+                    max_over_thr = max_cos_sim >= 0.995
+                    argmax_cos_sim = np.argmax(cos_sims, axis=1)
+
+                    # Count times that both thresholds were met
+                    sim_threshold_met = np.logical_and(min_under_thr, max_over_thr)
+                    # indices where policy and state agreed on most similar state
+                    policy_and_state_most_sim = argmax_cos_sim == argmin_MSE
+                    # Same indices
+                    sim_indices = np.logical_and(policy_and_state_most_sim.reshape(-1), sim_threshold_met.reshape(-1))
+                    total_similar_states_found += np.sum(sim_indices)
+
+                    num_sim_states = np.sum(max_over_thr)
+                    num_sim_policies = np.sum(min_under_thr)
+                    if np.sum(sim_threshold_met) > 0:
+                        print(f"# Times policy sim: {num_sim_policies}")
+                        print(f"# Times states sim: {num_sim_states}")
+                        print(f"# Times policy and states sim: {np.sum(sim_threshold_met)}")
+                        print(f"# Times policy and state most sim at action X: {np.sum(policy_and_state_most_sim)}")
+                        input(f"# Times policy and states sim @ same spot: {np.sum(sim_indices)}")
+
+                    # Structure info to pass to cpp code to reuse existing states
+                    new_policy = np.where(np.expand_dims(sim_indices, axis=1), gathered_policy, current_policy)
                     current_policy = new_policy
-                    reusing_node_indices = (min_under_thr * (argmin_MSE + 2) - 1).tolist()
-                    # print(f"reusing_node_indices: {reusing_node_indices}")
+                    reusing_node_indices = (sim_indices * (argmin_MSE + 2) - 1).tolist()
 
                 policy_softmax_pool.append(current_policy)
-                # input(policy_softmax_pool)
                 ############################
 
                 hidden_state_pool.append(hidden_state_nodes)
@@ -149,3 +217,5 @@ class MCTS(object):
                 tree.batch_back_propagate(reusing_node_indices, hidden_state_index_x, discount,
                                           value_prefix_pool, value_pool, policy_logits_pool,
                                           min_max_stats_lst, results, is_reset_lst)
+
+            print(f"Total similar states found: {total_similar_states_found}")
