1ce26f997b41369dde6f37a47dd59c172eb5e8d2
Matt-Harvill <m.harvill23@gmail.com>
2023-06-06 13:45:29 -0700

    added borders, cleaned data, condensed plot for dynamic branching

--- result_plot.py
+++ result_plot.py
@@ -29,7 +29,7 @@ for title in sorted_data:
 
 # Plotting
 for title, scores in sorted_data.items():
-    group = sorted(scores['group'])
+    group = scores['group']
     games = scores['games']
     num_games = len(games)
     num_group = len(group)
@@ -42,23 +42,24 @@ for title, scores in sorted_data.items():
     
     plt.figure(figsize=(10, 6))
     x = range(num_group)
-    width = 0.2
+    width = 0.6 / num_group
     
     # Sort the mean scores for each game by increasing search paths
     sorted_scores = []
     for game, mean_scores in games.items():
-        sorted_scores.append(sorted(mean_scores, key=lambda x: x[0]))  # Sort by increasing search paths
+        # sorted_scores.append(sorted(mean_scores, key=lambda x: x[0]))  # Sort by increasing search paths
+        sorted_scores.append(mean_scores)
     
     # Plot Mean Scores for each game
     handles = []
     labels = []
 
     color_arrays = [
-        ['#BDFCC9', '#87EBA2', '#55D982', '#1EA457'],
-        ['#CCEBFF', '#80C1FF', '#4DA8FF', '#267EFF'],
-        ['#FFD6E7', '#FFA3C2', '#FF80A6', '#FF5480'],
-        ['#E8D7FF', '#BEA7FF', '#956DFF', '#6732FF'],
-        ['#FFCCCC', '#FF8080', '#FF4D4D', '#FF2626'],
+        ['#D1F2EB', '#A3E4D7', '#76D7C4', '#48C9B0', '#1ABC9C', '#17A589', '#148F77', '#117864', '#0E6251', '#0B5345'],
+        ['#D6EAF8', '#AED6F1', '#85C1E9', '#5DADE2', '#3498DB', '#2E86C1', '#2874A6', '#21618C', '#1B4F72', '#154360'],
+        ['#FADBD8', '#F5B7B1', '#F1948A', '#EC7063', '#E74C3C', '#CB4335', '#B03A2E', '#943126', '#78281F', '#641E16'],
+        ['#E8DAEF', '#D2B4DE', '#BB8FCE', '#A569BD', '#8E44AD', '#7D3C98', '#6C3483', '#5B2C6F', '#4A235A', '#1F618D'],
+        ['#FFCCCC', '#FF9999', '#FF6666', '#FF3333', '#FF0000', '#CC0000', '#990000', '#660000', '#330000', '#000000'],
     ]
 
     # Generate a random index
@@ -73,12 +74,12 @@ for title, scores in sorted_data.items():
 
         # print(f"Game: {game}, Group: {group}, Mean Score: {mean_scores}")
         x_pos = [(j * width) + i - offset for j in x]
-        handles.append(plt.bar(x_pos, mean_scores, width, color=colors))
+        handles.append(plt.bar(x_pos, mean_scores, width, color=colors, edgecolor='black', linewidth=1))
         labels.append(game)
     
-    plt.xlabel('Game')
-    plt.ylabel('Mean Score')
-    plt.title(f'Mean Scores for {title}')
+    plt.xlabel('Game', fontweight='bold')
+    plt.ylabel('Mean Score', fontweight='bold')
+    plt.title(f'Mean Scores for {title}', fontweight='bold')
     
     # Set x-axis labels and tick positions
     plt.xticks(range(num_games), games.keys())
@@ -88,7 +89,7 @@ for title, scores in sorted_data.items():
     # Create legend with search paths information
     if legend:
         legend_colors = [plt.Rectangle((0, 0), 1, 1, color=color) for color in colors]
-        plt.legend(legend_colors, group)
+        plt.legend(legend_colors, group, edgecolor='black')
         # plt.legend(legend_colors, group, title='Group')
     
     # Show the plot
